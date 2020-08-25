c8bc91a6bf8f19f7cab815769d3cad4470867d7d
jisapa4 <philip.antin@jbhunt.com>
2020-08-24 19:55:18 -0500

    Set up actions; Configure path aliases

---
+++ src/app/shared/models/famous-quote.model.ts
@@ -0,0 +1,6 @@
+export interface FamousQuote {
+  id: number;
+  quoteTest: string;
+  source: string;
+  sourceMedium: string;
+}
---
+++ src/app/shared/store/actions/quotes.action.ts
@@ -0,0 +1,31 @@
+import { Action } from '@ngrx/store';
+import { FamousQuote } from '@sharedModels/famous-quote.model';
+
+export const enum ActionTypes {
+  QUOTES = '[Quotes] Get',
+  QUOTES_SUCCESS = '[Quotes] Get Success',
+  QUOTES_FAIL = '[Quotes] Get Fail',
+  UPDATE_QUOTE_BY_ID = '[Quotes] Update Quote by ID'
+}
+
+export class Quotes implements Action {
+  readonly type = ActionTypes.QUOTES;
+  constructor() {}
+}
+
+export class QuotesSuccess implements Action {
+  readonly type = ActionTypes.QUOTES_SUCCESS;
+  constructor(public payload: FamousQuote[]) {}
+}
+
+export class QuotesFail implements Action {
+  readonly type = ActionTypes.QUOTES_FAIL;
+  constructor(public payload: string) {}
+}
+
+export class UpdateQuoteById implements Action {
+  readonly type = ActionTypes.UPDATE_QUOTE_BY_ID;
+  constructor(public payload: FamousQuote) {}
+}
+
+export type LoadListAction = Quotes | QuotesSuccess | QuotesFail | UpdateQuoteById;
